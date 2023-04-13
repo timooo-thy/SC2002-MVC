a10c5e4587458d24e2d84b69848578a2e79d07e2
Timothy Lee <timothylhy@hotmail.com>
2023-04-13 14:08:22 +0800

    annotation for app package

--- src/app/FYPRSA.java
+++ src/app/FYPRSA.java
@@ -4,10 +4,22 @@ import controllers.MainController;
 import models.User;
 import utilities.Database;
 
+/**
+ * The FYPRSA class is the main class that initialises the FYP Registration System Application and runs the main controller.
+ * It also initialises and updates the application's database.
+ */
 public class FYPRSA {
 	
 	static User user;
 	
+	/**
+	 * The main method is the entry point for the FYP Registration System Application.
+	 * It initialises the MainController and calls its run method with the currently logged in user. 
+	 * It also initialises the application's database and updates it after the run method completes. 
+	 *
+	 * @param args The command line arguments passed to the application.
+	 * @throws Throwable Any exception that may occur during the execution of the program.
+	 */
 	public static void main(String args[]) throws Throwable  {
 		
 		final MainController mainController = new MainController();
