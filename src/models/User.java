892bdbd6e609e393f9ea6d6b6733a6a3c0d38d94
Timothy Lee <timothylhy@hotmail.com>
2023-04-13 15:51:24 +0800

    javadoc for users

--- src/models/User.java
+++ src/models/User.java
@@ -1,13 +1,36 @@
 package models;
 
+/**
+ * The User class is an abstract class that serves as the parent class for all user types in the FYP Registration System.
+ * It defines the common properties and methods of all users such as id, email address, name and password.
+ */
 public abstract class User {
 
+	/**
+	 * This method returns the user's ID.
+	 * 
+	 * @return The user's ID.
+	 */
 	public abstract String getId();
 
+	/**
+	 * This method returns the user's email address.
+	 * 
+	 * @return The user's email address.
+	 */
 	public abstract String getEmailAddress(); 
 
+	/**
+	 * This method returns the user's name.
+	 * 
+	 * @return The user's name.
+	 */
 	public abstract String getName();
 	
+	/**
+	 * This method returns the user's password.
+	 * @return The user's password.
+	 */
 	public abstract String getPassword();
 	
 }
