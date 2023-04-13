f0bc06dec5bd74f94b9af368eea96e2df4b3a7fe
Timothy Lee <timothylhy@hotmail.com>
2023-04-13 19:17:37 +0800

    student and supervisor javadoc

--- src/models/Student.java
+++ src/models/Student.java
@@ -2,29 +2,69 @@ package models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import utilities.Database;
 
+/**
+ * The Student class represents a student in the system.
+ * It extends the User class and provides methods to manage the list of students.
+ * It also has methods to retrieve and update information about the students from the database.
+ */
 public class Student extends User {
 	
+	/**
+	 * File path of the file to store the list of students.
+	 */
 	private static final String FILEPATH = "src/data/";
 	
+	/**
+	 * File name of the file to store the list of students.
+	 */
 	private static final String FILENAME = "studentList.txt";
 
+	/**
+	 * List of all the students in the system.
+	 */
 	private static ArrayList<Student> studentsList = new ArrayList<Student>();
 
+	/**
+	 * Database object to perform database operations.
+	 */
 	private static Database d = new Database();
 	
+	/**
+	 * Student's ID.
+	 */
 	private String studentId;
 	
+	/**
+	 * Student's Name
+	 */
 	private String studentName;
 	
+	/**
+	 * Student's Email
+	 */
 	private String studentEmail;
 	
+	/**
+	 * Student's Password
+	 */
 	private String studentPassword;
 
+	/**
+	 * Student's Project ID
+	 */
     private int projectID;
     
+    /**
+     * Creates a new Student object with the given attributes, and adds it to the list of students.
+     * 
+     * @param studentId The ID of the student.
+     * @param studentEmail The email address of the student.
+     * @param studentPassword The password of the student.
+     * @param studentName The name of the student.
+     * @param projectID The ID of the project that the student is assigned to.
+     */
     public Student(String studentId, String studentEmail, String studentPassword, String studentName, int projectID) {
 		this.studentId = studentId.toUpperCase();
 		this.studentName = studentName;
@@ -33,92 +73,169 @@ public class Student extends User {
 		this.projectID = projectID;
 		addStudent(this);
 	}
-
+    
+    /**
+     * Sets the name of the student.
+     * 
+     * @param studentName The new name of the student.
+     */
 	public void setStudentName(String studentName){
 		this.studentName = studentName;
 	}
 
+	/**
+	 * Retrieves the name of the student.
+	 * 
+	 * @return The name of the student.
+	 */
 	public String getName(){
 		return this.studentName;
 	}
 	
+	/**
+	 * Retrieves the email address of the student.
+	 * @return The email address of the student.
+	 */
 	public String getEmailAddress(){
 		return this.studentEmail;
 	}
 	
+	/**
+	 * Sets the email address of the student.
+	 * 
+	 * @param studentEmail The new email address of the student.
+	 */
 	public void setEmailAddress(String studentEmail){
 		this.studentEmail =  studentEmail;
 	}
 	
+	/**
+	 * Retrieves the password of the student.
+	 * 
+	 * @return The password of the student.
+	 */
 	public String getPassword(){
 		return this.studentPassword;
 	}
 	
+	/**
+	 * Sets the password of the student.
+	 * 
+	 * @param studentPassword The new password of the student.
+	 */
 	public void setPassword(String studentPassword){
 		this.studentPassword = studentPassword;
 	}
 	
+	/**
+	 * Adds a new student to the list of students, and updates the file that stores the list of students.
+	 * 
+	 * @param s The student object to add.
+	 */
 	public void addStudent(Student s) {
 		studentsList.add(s);
 		updateFile();
 	}
 	
+	/**
+	 * Retrieves the ID of the student.
+	 * 
+	 * @return The ID of the student.
+	 */
 	public String getId() {
 		return this.studentId;
 	}
 
+	/**
+	 * Retrieves the ID of the project that the student is assigned to.
+	 * 
+	 * @return The ID of the project that the student is assigned to.
+	 */
     public int getProjectID() {
 		return projectID;
 	}
 
+    /**
+     * Sets the ID of the project that the student is assigned to.
+     * 
+     * @param projectID The new project ID.
+     */
     public void setProjectID(int projectID) {
         this.projectID = projectID;
     }
 	
-	
+    /**
+     * Checks if there is already a student with the given ID in the list of students.
+     * 
+     * @param studentId The ID of the student to check.
+     * @return True if there is already a student with the given ID, false otherwise.
+     */
 	public static boolean duplicateStudentId(String studentId) {
-
 		for (Student s : studentsList) {
-
 			if (s.getId().equalsIgnoreCase(studentId)) {
 				return true;
 			}
 		}
-
 		return false;
 	}
 	
+	/**
+	 * Retrieves the total number of students in the list of students.
+	 * 
+	 * @return The total number of students in the list of students.
+	 */
 	public static int getTotalNumberOfStudents(){
 		return studentsList.size();
 	}
 	
+	/**
+	 * Retrieves the list of students.
+	 * 
+	 * @return The list of students.
+	 */
 	public static ArrayList<Student> getStudentsList(){
 		return studentsList;
 	}
 	
+	/**
+	 * Retrieves a specific student from the list of students.
+	 * 
+	 * @param i The index of the student to retrieve.
+	 * @return The student at the specified index.
+	 */
 	public static Student getStudent(int i){
 		return studentsList.get(i-1);
 	}
 	
+	/**
+	 * Removes a student from the list of students.
+	 * 
+	 * @param s The student to remove.
+	 */
 	public static void removeStudent(Student s){
 		studentsList.remove(s);
 	}
 	
+	/**
+	 * Generates a menu of students with their names and IDs.
+	 * 
+	 * @return An ArrayList of strings that represent the student menu.
+	 */
 	public static ArrayList<String> getStudentMenu(){
-		
 		ArrayList<String> studentMenu = new ArrayList<String>();
-		
 		for (Student s : studentsList) {
-
 			studentMenu.add(s.getName() + " ("
 					+ s.getId() + ")");
 		}
-		
 		return studentMenu;
 	}
-	
-	///////////////////////////////////////////////////////////////////////
 
+	/**
+	 * Returns the student object from a given student name.
+	 * 
+	 * @param studentName The name of the student to retrieve
+	 * @return The student object associated with the given name, or null if not found
+	 */
 	public static Student getStudentFromName(String studentName) {
 		for (Student s : studentsList) {
 			if (s.getName().equals(studentName)) {
@@ -128,8 +245,12 @@ public class Student extends User {
 		return null;
 	}
 		
-	 //return student object based on name
-
+	/**
+	 * Returns the student object from a given student ID.
+	 * 
+	 * @param studentID The ID of the student to retrieve
+	 * @return The student object associated with the given ID, or null if not found
+	 */
 	public static Student getStudentFromID(String studentID) {
 		for (Student s : studentsList) {
 			if (s.getId().equals(studentID)) {
@@ -138,9 +259,13 @@ public class Student extends User {
 		}
 		return null;
 	}	
-	//return student object based on id
-	
-	// Get Student Id or Name and Email based on Name or Id
+
+	/**
+	 * Returns the email address of a student with a given name.
+	 * 
+	 * @param studentName The name of the student whose email address to retrieve
+	 * @return The email address of the student with the given name, or null if not found
+	 */
 	public static String getStudentNameToEmail(String studentName){
 		for (Student stud : Student.getStudentsList()) {
 			if ((stud.getName()).equals(studentName)) {
@@ -150,6 +275,12 @@ public class Student extends User {
 		return null;
 	}
 	
+	/**
+	 * Returns the ID of a student with a given name.
+	 * 
+	 * @param studentName The name of the student whose ID to retrieve
+	 * @return The ID of the student with the given name, or null if not found
+	 */
 	public static String getStudentNameToId(String studentName) {
 		for (Student stud : Student.getStudentsList()) {
 			if ((stud.getName()).equals(studentName)) {
@@ -159,6 +290,12 @@ public class Student extends User {
 		return null;
 	}
 	
+	/**
+	 * Returns the email address of a student with a given ID.
+	 * 
+	 * @param studentId The ID of the student whose email address to retrieve
+	 * @return The email address of the student with the given ID, or null if not found
+	 */
 	public static String getStudentIdToEmail(String studentId){
 		for (Student stud : Student.getStudentsList()) {
 			if ((stud.getId()).equals(studentId)) {
@@ -168,6 +305,12 @@ public class Student extends User {
 		return null;
 	}
 	
+	/**
+	 * Returns the name of a student with a given ID.
+	 * 
+	 * @param studentId the ID of the student whose name to retrieve
+	 * @return the name of the student with the given ID, or null if not found
+	 */
 	public static String getStudentIdToName(String studentId) {
 		for (Student stud : Student.getStudentsList()) {
 			if ((stud.getId()).equals(studentId)) {
@@ -177,12 +320,20 @@ public class Student extends User {
 		return null;
 	}
 	
-	///////////////////////////////////////////////////////////////////////
-	
+	/**
+	 * Sets the list of students to the specified list.
+	 * 
+	 * @param s The new list of students
+	 */
 	public static void updateStudentsList(ArrayList<Student> s){
 		studentsList = s;
 	}
-
+	
+	/**
+	 * Initializes the list of students from the data in the student data file.
+	 * 
+	 * @throws Throwable If there is an error reading the student data file
+	 */
 	public static void initializeFile() throws Throwable {
 		HashMap<String, String[]> map  = d.initializeStudentFile(FILENAME, FILEPATH);
 		for (String name : map.keySet()) {
@@ -193,6 +344,9 @@ public class Student extends User {
         }
 	}
 
+	/**
+	 * Updates the student data file with the current list of students.
+	 */
 	public static void updateFile() {
 		d.updateStudentFile(FILENAME,FILEPATH,studentsList);
 	}
--- src/models/Supervisor.java
+++ src/models/Supervisor.java
@@ -2,31 +2,74 @@ package models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import utilities.Database;
 
+/**
+ * This class represents a Supervisor who can supervise projects in the system.
+ * It extends the User class and contains a list of supervised projects and methods to add and remove them.
+ * It also has methods to retrieve and update information about the supervisors from the database.
+ */
 public class Supervisor extends User {
 	
+	/**
+	 * File path of the file to store the list of supervisors.
+	 */
 	private static final String FILEPATH = "src/data/";
 	
+	/**
+	 * File name of the file to store the list of supervisors.
+	 */
 	private static final String FILENAME = "facultyList.txt";
 
+	/**
+	 * List of all the supervisors in the system.
+	 */
 	private static ArrayList<Supervisor> supervisorsList = new ArrayList<Supervisor>();
 
+	/**
+	 * List of all the supervised projects of each supervisor in the system.
+	 */
 	private ArrayList<Project> supervisedProjectList = new ArrayList<Project>(0);
 	
+	/**
+	 * Database object to perform database operations.
+	 */
 	private static Database d = new Database();
 	
+	/**
+	 * Supervisor's ID.
+	 */
 	private String supervisorId;
 	
+	/**
+	 * Supervisor's Name.
+	 */
 	private String supervisorName;
 	
+	/**
+	 * Supervisor's Email.
+	 */
 	private String supervisorEmail;
 	
+	/**
+	 * Supervisor's Password.
+	 */
 	private String supervisorPassword;
 
+	/**
+	 * Supervisor's Project ID.
+	 */
     private int projectID;
 
+    /**
+     * Creates a new Supervisor object with the given attributes, and adds it to the list of supervisors.
+     * 
+     * @param supervisorId The ID of the supervisor.
+     * @param supervisorEmail The email address of the supervisor.
+     * @param supervisorPassword The password of the supervisor.
+     * @param supervisorName The name of the supervisor.
+     * @param projectID The ID of the project that the supervisor is assigned to.
+     */
     public Supervisor(String supervisorId, String supervisorEmail, String supervisorPassword, String supervisorName) {
 		this.supervisorId = supervisorId.toUpperCase();
 		this.supervisorName = supervisorName;
@@ -36,52 +79,112 @@ public class Supervisor extends User {
 		addSupervisor(this);
 	}
 
+    /**
+     * Sets the name of the supervisor.
+     * 
+     * @param supervisorName The new name of the supervisor.
+     */
 	public void setSupervisorName(String supervisorName){
 		this.supervisorName = supervisorName;
 	}
 
+	/**
+	 * Retrieves the name of the supervisor.
+	 * 
+	 * @return The name of the supervisor.
+	 */
 	public String getName(){
 		return this.supervisorName;
 	}
 	
+	/**
+	 * Retrieves the email address of the supervisor.
+	 * @return The email address of the supervisor.
+	 */
 	public String getEmailAddress(){
 		return this.supervisorEmail;
 	}
 	
+	/**
+	 * Sets the email address of the supervisor.
+	 * 
+	 * @param supervisorEmail The new email address of the supervisor.
+	 */
 	public void setEmailAddress(String supervisorEmail){
 		this.supervisorEmail =  supervisorEmail;
 	}
 	
+	/**
+	 * Retrieves the password of the supervisor.
+	 * 
+	 * @return The password of the supervisor.
+	 */
 	public String getPassword(){
 		return this.supervisorPassword;
 	}
 	
+	/**
+	 * Sets the password of the supervisor.
+	 * 
+	 * @param supervisorPassword The new password of the supervisor.
+	 */
 	public void setPassword(String supervisorPassword){
 		this.supervisorPassword = supervisorPassword;
 	}
 	
+	/**
+	 * Adds a new supervisor to the list of supervisors, and updates the file that stores the list of supervisors.
+	 * 
+	 * @param s The supervisor object to add.
+	 */
 	public void addSupervisor(Supervisor s) {
 		supervisorsList.add(s);
 		updateFile();
 	}
 	
+	/**
+	 * Retrieves the ID of the supervisor.
+	 * 
+	 * @return The ID of the supervisor.
+	 */
 	public String getId() {
 		return this.supervisorId;
 	}
 
+	/**
+	 * Retrieves the ID of the project that the supervisor is assigned to.
+	 * 
+	 * @return The ID of the project that the supervisor is assigned to.
+	 */
     public int getProjectID() {
 		return projectID;
 	}
 
-    /*public setProjectID(int projectID) {
+    /**
+     * Sets the ID of the project that the supervisor is assigned to.
+     * 
+     * @param projectID The new project ID.
+     */
+    public void setProjectID(int projectID) {
         this.projectID=projectID;
-    }*/
+    }
 	
+    /**
+     * Returns the list of projects supervised by this supervisor.
+     * 
+     * @return The list of projects supervised by this supervisor
+     */
     public ArrayList<Project> getSupervisedProjectList(){
     	return this.supervisedProjectList;
     }
     
-    
+    /**
+     * Returns the list of projects supervised by the supervisor with the given ID.
+     * 
+     * @param supervisorId The ID of the supervisor whose projects are being retrieved
+     * @return The list of projects supervised by the supervisor with the given ID
+     */
+
     public static ArrayList<Project> getSupervisedProjectList(String supervisorId){
     	for (Supervisor sup : Supervisor.getSupervisorsList())
     		if (sup.getId().equals(supervisorId))
@@ -89,8 +192,12 @@ public class Supervisor extends User {
     	return null;
     }
     
- 	//////////////////////////////////////////////////////////////////////////////////////
-
+    /**
+     * Returns the Supervisor object with the given ID.
+     * 
+     * @param supervisorId the ID of the Supervisor being retrieved
+     * @return the Supervisor object with the given ID
+     */
     public static Supervisor getSupervisorFromId(String supervisorId) {
     	for (Supervisor sup : supervisorsList) {
     		if (sup.getId().equals(supervisorId)) 
@@ -99,6 +206,12 @@ public class Supervisor extends User {
     	return null;
     }
     
+    /**
+     * Returns the supervisor object with the given name.
+     * 
+     * @param supervisorName The name of the Supervisor being retrieved
+     * @return The supervisor object with the given name
+     */
     public static Supervisor getSupervisorFromName(String supervisorName) {
     	for (Supervisor sup : supervisorsList) {
     		if (sup.getName().equals(supervisorName)) 
@@ -107,7 +220,12 @@ public class Supervisor extends User {
     	return null;
     }
     
- // Get Supervisor Id or Name and Email based on Name or Id
+    /**
+     * Returns the email address of the supervisor with the given name.
+     * 
+     * @param supervisorName The name of the supervisor whose email address is being retrieved
+     * @return The email address of the supervisor with the given name
+     */
  	public static String getSupervisorNameToEmail(String supervisorName){
  		for (Supervisor sup : Supervisor.getSupervisorsList()) {
  			if (sup.getName().equals(supervisorName)) {
@@ -117,6 +235,12 @@ public class Supervisor extends User {
  		return null;
  	}
  	
+ 	/**
+ 	 * Returns the ID of the supervisor with the given name.
+ 	 * 
+ 	 * @param supervisorName The name of the supervisor
+ 	 * @return The ID of the supervisor with the given name, or null if no such supervisor exists
+ 	 */
  	public static String getSupervisorNameToId(String supervisorName) {
  		for (Supervisor sup : Supervisor.getSupervisorsList()) {
  			if (sup.getName().equals(supervisorName)) {
@@ -126,6 +250,12 @@ public class Supervisor extends User {
  		return null;
  	}
  	
+ 	/**
+ 	 * Returns the email address of the supervisor with the given ID.
+ 	 * 
+ 	 * @param supervisorId The ID of the supervisor
+ 	 * @return The email address of the supervisor with the given ID, or null if no such supervisor exists
+ 	 */
  	public static String getSupervisorIdToEmail(String supervisorId){
  		for (Supervisor sup : Supervisor.getSupervisorsList()) {
  			if (sup.getId().equals(supervisorId)) {
@@ -135,6 +265,12 @@ public class Supervisor extends User {
  		return null;
  	}
  	
+ 	/**
+ 	 * Returns the name of the supervisor with the given ID.
+ 	 * 
+ 	 * @param supervisorId The ID of the supervisor
+ 	 * @return The name of the supervisor with the given ID, or null if no such supervisor exists
+ 	 */
  	public static String getSupervisorIdToName(String supervisorId) {
  		for (Supervisor sup : Supervisor.getSupervisorsList()) {
  			if (sup.getId().equals(supervisorId)) {
@@ -143,54 +279,87 @@ public class Supervisor extends User {
  		}
  		return null;
  	}
- 		
- 	//////////////////////////////////////////////////////////////////////////////////////
  	
+ 	/**
+ 	 * Checks if a supervisor with the given ID already exists in the list of supervisors.
+ 	 * 
+ 	 * @param supervisorId The ID of the supervisor to check for duplicates
+ 	 * @return True if a supervisor with the given ID already exists, false otherwise
+ 	 */
 	public static boolean duplicateSupervisorId(String supervisorId) {
-
 		for (Supervisor s : supervisorsList) {
-
 			if (s.getId().equalsIgnoreCase(supervisorId)) {
 				return true;
 			}
 		}
-
 		return false;
 	}
 	
+	/**
+	 * Returns the total number of supervisors in the list.
+	 * 
+	 * @return The total number of supervisors in the list
+	 */
 	public static int getTotalNumberOfSupervisors(){
 		return supervisorsList.size();
 	}
 	
+	/**
+	 * Returns a list of all supervisors.
+	 * 
+	 * @return A list of all supervisors
+	 */
 	public static ArrayList<Supervisor> getSupervisorsList(){
 		return supervisorsList;
 	}
 	
+	/**
+	 * Returns the supervisor at the specified index in the list.
+	 * 
+	 * @param i The index of the supervisor to return
+	 * @return The supervisor at the specified index in the list
+	 */
 	public static Supervisor getSupervisor(int i){
 		return supervisorsList.get(i);
 	}
 	
+	/**
+	 * Removes the specified supervisor from the list.
+	 * 
+	 * @param s The supervisor to remove from the list
+	 */
 	public static void removeSupervisor(Supervisor s){
 		supervisorsList.remove(s);
 	}
 	
+	/**
+	 * Generates a menu of supervisors with their names and IDs.
+	 * 
+	 * @return An ArrayList of strings that represent the student menu.
+	 */
 	public static ArrayList<String> getSupervisorMenu(){
-		
 		ArrayList<String> supervisorMenu = new ArrayList<String>();
-		
 		for (Supervisor s : supervisorsList) {
-
 			supervisorMenu.add(s.getName() + " ("
 					+ s.getId() + ")");
 		}
-		
 		return supervisorMenu;
 	}
 
+	/**
+	 * Sets the list of supervisors to the specified list.
+	 * 
+	 * @param s The new list of supervisors
+	 */
 	public static void updateSupervisorsList(ArrayList<Supervisor> s){
 		supervisorsList = s;
 	}
 
+	/**
+	 * Initializes the list of supervisors from the data in the supervisor data file.
+	 * 
+	 * @throws Throwable If there is an error reading the supervisor data file
+	 */
 	public static void initializeFile() throws Throwable {
 		HashMap<String, String[]> map  = d.initializeFile(FILENAME, FILEPATH);
 		for (String name : map.keySet()) {
@@ -199,9 +368,11 @@ public class Supervisor extends User {
         }
 	}
 
+	/**
+	 * Updates the supervisor data file with the current list of students.
+	 */
 	public static void updateFile() {
 		ArrayList<User> usersList = new ArrayList<>(supervisorsList);
-		
 		d.updateFile(FILENAME,FILEPATH,usersList);
 	}
 
